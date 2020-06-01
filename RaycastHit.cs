6892a9e9a51cf116cf734afc06baeded7d3d721a
Joaquim2009 <65865569+Joaquim2009@users.noreply.github.com>
2020-06-01 08:37:28 +0200

    Update RaycastHit.cs

--- RaycastHit.cs
+++ RaycastHit.cs
@@ -1,6 +1,6 @@
 RaycastHit hit;
 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-if(Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+if(Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask) && Input.GetButtonDown(0))
 {
   //Do Something
 }
